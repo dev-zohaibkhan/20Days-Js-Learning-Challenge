3bf4d112b3092ed50f8804b88afe62b77fa5bd85
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-02 23:14:42 +0500

    write some code in object file

--- object.js
+++ object.js
@@ -8,10 +8,35 @@ let std = {
     pass:2233
 }
 
-console.log(std.firstName);
-console.log(std["last Name"]); // i reffer this way
-console.log(typeof std.stdClass); // it's not working but no problem
+// console.log(std.firstName);
+// console.log(std["last Name"]); // i reffer this way
+// console.log(typeof std.stdClass); // it's not working but no problem
 
+let teacher = {
+    firstName : "zaid",
+    lasatName : "khan",
+    email : "teacher@google.com",
+    id : 2299,
+    pass : 1234
+}
 
+let obj1= {
+    1 :"a",
+    2 : "b",
+}
+let obj2= {
+    3 :"a",
+    4 : "b",
+}
+let obj3= {
+    5 :"a",
+    6 : "b",
+}
+
+// let combine = Object.assign(obj1 , obj2, obj3)
+// console.log(combine);
+
+let add = {...obj1, ...obj2, ...obj3}
+console.log(add);
 
 
