9a9cc34509d6d96191aca469cdfe18b9ffe04bbc
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-02 22:23:50 +0500

    write some code inside array file

--- array.js
+++ array.js
@@ -3,9 +3,38 @@ programmingLang.push("Java", "C#")
 programmingLang.pop()
 programmingLang.unshift("Rust")
 programmingLang.shift()
-let slice = programmingLang.slice(0, 3)
-console.log(slice);
-console.log(programmingLang)
-let splice = programmingLang.splice(1,2)
-console.log(splice)
-console.log(programmingLang)
+// let slice = programmingLang.slice(0, 3)
+// console.log(slice);
+// console.log(programmingLang)
+// let splice = programmingLang.splice(1,2)
+// console.log(splice)
+// console.log(programmingLang)
+
+let otherComputerLang = ["HTML", "Css"];
+// let mergeArray = programmingLang.concat(otherComputerLang)
+// console.log(mergeArray);
+
+// let addTwoArray =[...programmingLang, ...otherComputerLang]
+// console.log(addTwoArray);
+// let num = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10, [11, 12, [13, 14, [15, 16]]]]];
+// let oneArray = num.flat(Infinity)
+// console.log(oneArray);
+// console.log(num);
+// let name = "zohaib";
+// let nameArray = Array.from(name)
+// console.log(nameArray);
+// console.log(name);
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+let score4 = 400;
+let score5 = 500;
+
+let oneScore = Array.of(score1, score2, score3, score4, score5)
+console.log(oneScore);
+
+
+
+
+
