8e23d4b6650fe84bc45c7c46924830475649bc2e
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-04 21:12:42 +0500

    added control-flow file and write a program

---
+++ control-flow.js
@@ -0,0 +1,15 @@
+let user = "zohaib";
+let pass = 1122;
+
+if(user === "zohaib" && pass === 1122){
+    let otp = 4567;
+    if(otp === 4567){
+        console.log("Welcome to Dashboard!");
+    }else{
+        console.log("You Entered Wrong OTP!");
+        
+    }
+}else{
+    console.log("You Entered Wrong Username/Password");
+    
+}
