80abf3f9518b2bb38b8928481409a13c51676c40
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-04 21:29:03 +0500

    write a program using switch statments

--- control-flow.js
+++ control-flow.js
@@ -13,3 +13,33 @@ if(user === "zohaib" && pass === 1122){
     console.log("You Entered Wrong Username/Password");
     
 }
+
+// Switch Statments
+let day = "Frieday";
+
+switch(day){
+    case "Sunday":
+        console.log("today is Sunday");
+    break
+    case "Monday":
+        console.log("today is Monday");        
+    break
+    case "Tuesday":
+        console.log("today is Tuesday");        
+    break
+    case "Wednesday":
+        console.log("today is Wednesday");        
+    break
+    case "Thersday":
+        console.log("today is Thersday");        
+    break
+    case "Frieday":
+        console.log("today is Frieday");        
+    break
+    case "Saturday":
+        console.log("today is Saturday");        
+    default:
+        console.log("Plese enter the correct Day!");
+        break
+
+}
