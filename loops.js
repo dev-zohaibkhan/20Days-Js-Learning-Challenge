7fc20cefe553e4196a87a25135c299141ddbe3e9
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-05 11:37:06 +0500

    Add loops.js file and added some pyramid programs

---
+++ loops.js
@@ -0,0 +1,14 @@
+// for loop
+// Half Pyramid using Js
+
+for(let i = 1; i <=10; i++){
+    console.log("* ".repeat(i)); 
+}
+    console.log("Break....");
+    
+// Pyramid Using Js
+let n = 10;
+for(let i = 1; i <= n; i++){
+    console.log("   ".repeat((n - i)) + " * ".repeat(i * 2 - 1));
+   
+}
