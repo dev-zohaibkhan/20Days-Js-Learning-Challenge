da6e6ec85aac1fe3866f44f224ba3e71849afb71
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-05 11:57:11 +0500

    writed a continue & break statements and print something new

--- loops.js
+++ loops.js
@@ -10,5 +10,33 @@ for(let i = 1; i <=10; i++){
 let n = 10;
 for(let i = 1; i <= n; i++){
     console.log("   ".repeat((n - i)) + " * ".repeat(i * 2 - 1));
-   
+}
+
+console.log("break...");
+
+// Print something new
+
+let items = ["books", "pens", "papers", "copies"];
+for(let index = 0; index < items.length; index++){
+    console.log(`the items is ${items[index]}`);
+}
+
+// break and continue Statments
+// break Statments
+
+for(let i= 1; i<5; i++){
+    if(i == 3){
+        console.log("Breaked");
+        break // the value is not printing after apply break statments
+    }
+    console.log(`the value is ${i}`);
+}
+
+// Continue Statments
+for(let i= 1; i<5; i++){
+    if(i == 3){
+        console.log("Continued");
+        continue // the value is not printing after apply break statments
+    }
+    console.log(`the value is ${i}`);
 }
