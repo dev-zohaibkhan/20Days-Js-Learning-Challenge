9909e1cc309210b47d9af601e4d256af1b17a681
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-08 20:21:14 +0500

    writed reduce and map method

--- filter-map.js
+++ filter-map.js
@@ -5,5 +5,23 @@ let storeNum = numbers.filter((num) => num > 3) // suing implicit
 let otherNum = numbers.filter((num) => {
     return num > 5 // in under a scope the return keyword is manadarity
 })
-console.log(otherNum);
-console.log(numbers)
+// console.log(otherNum);
+// console.log(numbers)
+
+// map  not changing on original array 
+let num = [1, 2, 3, 4, 5, 6, 7, 8]
+let newNums = num.map((eNum) => (eNum * 2))
+// console.log(newNums);
+
+// method channing mean apply multiple map are same method like filter and etc and each item
+let newNumber = num.map((eachNum) => eachNum * 3).map((eachNum) => eachNum + 2).filter((eachNum) => eachNum > 10)
+// console.log(newNumber);
+
+// reduce method 
+let initalVal = 1;
+let totalNum = num.reduce((acc, currVal) => {
+    console.log(`acc is ${acc} and curr value is ${currVal}`);  
+    return acc + currVal
+}, initalVal)
+console.log(totalNum);
+
