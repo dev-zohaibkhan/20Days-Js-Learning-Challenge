e75e993f0d0611591ae76fd57f13b8de8a0f3ca8
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-23 14:43:19 +0500

    write async await function

--- promises.js
+++ promises.js
@@ -1,14 +1,42 @@
-let createPromise = new Promise(function(resolve, reject){
-    setTimeout(() => {
-      console.log("Promise Work")  
+// let createPromise = new Promise( function(resolve, reject){
+//     setTimeout(() => {
+//       console.log("Promise Work")  
+//       resolve({
+//         name: "zohaib khan",
+//         id: 22
+//       }) // if you don't use this method the then is not connect to promise
+//     }, 2000);
+// })
+// createPromise.then(function(userDetails){
+//     console.log("yes then is connected")
+//     console.log(userDetails)
+// })
+// console.log("i tested the async programming here!")
+
+function getUser(){
+  return new Promise(function(resolve, reject){
+  setTimeout(function(){
+    let error = true;
+    if(!error){
       resolve({
-        name: "zohaib khan",
-        id: 22
-      }) // if you don't use this method the then is not connect to promise
-    }, 2000);
+        userName: "zohaib",
+        userEmail: "zohaib@just.com"
+      })
+    }else{
+      reject("ERROR: JS went wrong")
+    }
+  }, 1000)
 })
-createPromise.then(function(userDetails){
-    console.log("yes then is connected")
-    console.log(userDetails)
-})
-console.log("i tested the async programming here!")
+}
+
+async function fetchUser() {
+  try {
+    let user = await getUser();
+    console.log(user);
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchUser();
